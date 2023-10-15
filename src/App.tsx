884f79feefc2385b57ba2150724cd27315d6db73
Fran <kezicfran@gmail.com>
2023-10-15 04:05:08 +0200

    Login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../lib/firebase';
+import LoginForm from './components/LoginForm';
+
 function App() {
+  const [count, setCount] = useState<number>();
+
+  async function getCount() {
+    const countRef = doc(db, 'count', 'first');
+    const countSnap = await getDoc(countRef);
+    if (countSnap.exists()) setCount(countSnap.data().count);
+  }
+
+  useEffect(() => {
+    getCount();
+  }, []);
+  function handleButtonClick() {}
+
   return (
     <>
-      <h1>Welcome</h1>
+      <LoginForm />
+      <button onClick={handleButtonClick}>Increase count</button>
+      {count ? count : ''}
     </>
   );
 }
